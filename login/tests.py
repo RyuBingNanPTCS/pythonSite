62ff35c29d807742feb5c4e06237a72b4e7a00a6
RyuBingNan <tohoweb@hotmail.com>
2022-04-03 21:35:23 +0900

    登録画面とユーザー一覧画面が対応済み

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Login test
+import unittest
+import requests
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def __init__(self, methodName, url, params) -> None:
+        super().__init__(methodName)
+        self.params = params
+
+    def setUp(self):
+        self.params={
+            'username': 777,
+            'password': 123
+        }
+
+        self.url= 'http://localhost:8000/login/login'
+
+        # self.url= 'http://www.baidu.com/'
+
+    def test_login(self):
+        res = requests.get(url=self.url, params=self.params)
+        print(res.text)
+        self.assertEqual(200,res.status_code)
+        print(res.text)
+
+    def test_reg(self):
+        response = requests.post(url=self.url, data=self.params)
+        self.assertEqual(200,response.status_code)
+        print(response.text)
+    
+    def test_index(self):
+        response = requests.get(url=self.url)
+        self.assertEqual(200,response.status_code)
+        print(response.text)
+
+    def test_modify(self):
+        response = requests.ost(url=self.url, data=self.params)
+        self.assertEqual(200,response.status_code)
+        print(response.text)
+
+if __name__ == '__main__':
+    unittest.main()
+
