8bb6ced302b1b87c31afa4f4f28c145a3395187e
tsujimoto <keita.tsujimoto@persol.co.jp>
2022-04-12 15:57:23 +0900

    login画面追加

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <input v-model="userId" />
+    <input v-model="password" />
+
+    <button @click="login()" :disabled="!userId">ログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userId: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$store.commit('setUserId', this.userId) // (1)
+      this.$store.commit('setPassword', this.password) 
+      this.$router.push(this.$route.query.redirect) // (2)
+    }
+  }
+}
+</script>
